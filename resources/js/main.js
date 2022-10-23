0ffbfaa40929b61d61b04593be3b1d308c4cbf9a
Matthew_Cash <matthew@matthew-cash.com>
2022-10-23 15:33:46 -0700

    Make empty tab area draggabe

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,25 +1,30 @@
 (() => {
     const run = window => {
         const { document } = window;
+        let oldTabCount = 0;
 
-        const callback = mutations => {
-            for (const mutation of mutations) {
-                if (mutation.type !== 'attributes') continue;
-                if (mutation?.target?.getAttribute('primary') !== 'true') continue;
+        const onTabUpdate = () => {
+            const inNewTab = ['about:newtab', 'about:blank'].includes(window.gBrowser.currentURI.spec);
 
-                let inNewTab = false;
-                try {
-                    inNewTab = mutation.target.contentPartitionedPrincipal?.URI?.asciiSpec === 'about:newtab';
-                } catch { };
+            document.querySelector('#tabbrowser-tabpanels').style.visibility = inNewTab ? 'hidden' : 'visible';
+            document.querySelector('#main-window').style.backgroundImage = inNewTab ? 'url("chrome://resources/content/images/background.svg")' : null;
+            document.querySelector('#tabbrowser-tabbox').style['-moz-window-dragging'] = inNewTab ? 'drag' : 'no-drag';
 
-                console.log({ inNewTab });
-                document.querySelector('#tabbrowser-tabpanels').style.visibility = inNewTab ? 'hidden' : 'visible';
-                document.querySelector('#main-window').style.backgroundImage = inNewTab ? 'url("chrome://resources/content/images/background.svg")' : null;
-                document.querySelector('#tabbrowser-tabbox').style['-moz-window-dragging'] = inNewTab ? 'drag' : 'no-drag';
-            }
+            const tabCount = window.gBrowser.tabs.length;
+            const sidebarHeight = (tabCount + 1) * 38;
+            if (sidebarHeight > 1) document.querySelector('#sidebar').style.maxHeight = `${sidebarHeight}px`;
+
+            oldTabCount = tabCount;
         };
 
-        new window.MutationObserver(callback).observe(document.querySelector('#tabbrowser-tabpanels'), { attributes: true, childList: true, subtree: true });
+        new window.MutationObserver(onTabUpdate).observe(
+            window._gBrowser.tabContainer,
+            { attributes: true, childList: true, subtree: true }
+        );
+
+        const draggable = document.createElement('div');
+        draggable.id = 'sidebar-draggable';
+        document.querySelector('#sidebar-box').appendChild(draggable);
 
         console.log('Script loaded');
     };
